881d1350d7f62c23aeb8cfdf8ac00212309dfc8f
Conrado916645 <36912212+Conrado916645@users.noreply.github.com>
2020-04-27 14:43:09 +0800

    incorporate audioplayers package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
   }
 }
