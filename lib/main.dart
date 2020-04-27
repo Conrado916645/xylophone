c43f95e503ef7dcd5ba841226a2a6b90a2c01f16
Conrado916645 <36912212+Conrado916645@users.noreply.github.com>
2020-04-27 15:44:41 +0800

    play music and expanded button

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static AudioCache player = AudioCache();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    player.play('note1.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    player.play('note2.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    player.play('note3.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    player.play('note4.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.teal,
+                  onPressed: () {
+                    player.play('note5.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    player.play('note6.wav');
+                  },
+                  child: null,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    player.play('note7.wav');
+                  },
+                  child: null,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
